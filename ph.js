a341d75ca3ab78b54e82408e4700d12456a2785f
Peyal Hasan <peyalhasan143@gmail.com>
2025-09-26 20:54:59 +0600

    Data type

--- ph.js
+++ ph.js
@@ -3,4 +3,14 @@ console.log(age)
 const eggPrice = 10;
 const onionPrice = 20;
 const total = eggPrice + onionPrice
-console.log(total)
+console.log(total) 
+console.log(11%7)
+console.log(10 % 3)
+
+
+const passed = false;
+console.log(typeof passed);
+const country = "Bangladesh";
+const price = 555 ;
+console.log(typeof country)
+console.log(typeof price)
